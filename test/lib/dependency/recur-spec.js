8f553622b7d2e24112007a68b48d23125f5326fb
arrowrowe <arrowrowe@gmail.com>
2015-09-22 22:00:21 +0800

    Support working out dependencies

---
+++ test/lib/dependency/recur-spec.js
@@ -0,0 +1,44 @@
+var utilRecur = require('../../../lib/dependency/recur');
+var expect = require('expect');
+
+describe('Recur dependencies', function () {
+
+  function T(pkgs, name, recurred) {
+    expect(utilRecur(pkgs, pkgs[name].dependencies)).toEqual(recurred);
+  }
+
+  it('Empty', function () {
+    T({'a': {}}, 'a', []);
+    T({'a': {dependencies: []}}, 'a', []);
+  });
+
+  it('Package not found', function () {
+    expect(function () {
+      T({
+        'a': {},
+        'b': {
+          dependencies: ['a', 'c']
+        },
+        'c': {
+          dependencies: ['a', 'd']
+        }
+      }, 'b');
+    }).toThrow(/^Package \[d\] not found!$/);
+  });
+
+  it('Normal use, including omitted or empty dependencies', function () {
+    T({
+      'a': {},
+      'b': {
+        dependencies: ['a', 'c']
+      },
+      'c': {
+        dependencies: ['a', 'd']
+      },
+      'd': {
+        dependencies: []
+      }
+    }, 'b', ['a', 'c', 'd']);
+  });
+
+});
