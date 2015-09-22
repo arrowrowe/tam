ea710bf3934b9507bff39c756871b65b1793b675
arrowrowe <arrowrowe@gmail.com>
2015-09-23 00:15:28 +0800

    Revert "Support classifying i/o files"
    This reverts commit 89ca2b66eab1f88cf33f85eb3cc85b7e5f66dbc0.

--- test/lib/file/classify-spec.js
+++ test/lib/file/classify-spec.js
@@ -8,52 +8,26 @@ describe('Classify different types of files', function () {
   }
 
   it('Normal use', function () {
-    T({
-      input: [
+    T([
+      'a.js',
+      'b/a.js',
+      'b/b.json',
+      'b/b.scss',
+      'b/c/a.css'
+    ], {
+      js: [
         'a.js',
-        'b/a.js',
-        'b/b.json',
-        'b/b.scss',
+        'b/a.js'
+      ],
+      css: [
         'b/c/a.css'
       ],
-      output: [
-        'a-1.js',
-        'a-2.js',
-        'b.json',
-        'b.scss',
-        'a.css'
+      scss: [
+        'b/b.scss'
+      ],
+      else: [
+        'b/b.json'
       ]
-    }, {
-      input: {
-        js: [
-          'a.js',
-          'b/a.js'
-        ],
-        css: [
-          'b/c/a.css'
-        ],
-        scss: [
-          'b/b.scss'
-        ],
-        else: [
-          'b/b.json'
-        ]
-      },
-      output: {
-        js: [
-          'a-1.js',
-          'a-2.js'
-        ],
-        css: [
-          'a.css'
-        ],
-        scss: [
-          'b.scss'
-        ],
-        else: [
-          'b.json'
-        ]
-      }
     });
   });
 
