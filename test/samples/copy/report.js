f7ccc9008f087eb85bff67cf363cd8aed60802a0
arrowrowe <arrowrowe@gmail.com>
2015-09-25 16:10:18 +0800

    Update test: add scss in sample copy

--- test/samples/copy/report.js
+++ test/samples/copy/report.js
@@ -15,6 +15,11 @@ module.exports = {
       'behavior': 'copy',
       'files': r(['b/a.css', 'b/a.js', 'b/b.json', 'b/c/a.css', 'b/c/a.js']),
       'output': ['fake/another-pkg/a.css', 'fake/another-pkg/a.js', 'fake/another-pkg/b.json', 'fake/another-pkg/c/a.css', 'fake/another-pkg/c/a.js']
+    }, {
+      'behavior': 'compile',
+      'args': {'pretty': true },
+      'files': r(['b/b.scss']),
+      'output': ['fake/another-pkg/b.css']
     }]
   }
 };
