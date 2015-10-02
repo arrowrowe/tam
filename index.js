302ea570bf0d6d3976f29592b19a40040746b2e4
arrowrowe <arrowrowe@gmail.com>
2015-10-02 22:13:42 +0800

    Add log4js to warn errors for compilers or compressors

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var tam = {
   version: require('./package.json').version,
   prepare: require('./lib/prepare'),
   link: require('./lib/link'),
-  worker: require('./lib/worker')
+  worker: require('./lib/worker'),
+  log: require('./lib/log')
 };
 
 tam.tools = tam.worker.tools;
