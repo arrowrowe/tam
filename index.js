38e97bdbaa000617db14febedd8b475b7f571e2b
arrowrowe <arrowrowe@gmail.com>
2015-10-04 12:41:04 +0800

    Seperate tam.run to lib/cli/run

--- index.js
+++ index.js
@@ -1,49 +1,13 @@
-var fs = require('fs');
-var read = require('./lib/file/read');
-
-var PACKAGE = require('./package');
-
 var tam = {
-  version: PACKAGE.version,
+  version: require('./package').version,
   prepare: require('./lib/prepare'),
   link: require('./lib/link'),
   worker: require('./lib/worker'),
-  log: require('./lib/log')
+  log: require('./lib/log'),
+  run: require('./lib/cli/run')
 };
 
 tam.tools = tam.worker.tools;
 tam.build = tam.worker.build;
-
-tam.run = function (option) {
-
-  option = option || {};
-
-  var title = PACKAGE.name + ' @ ' + PACKAGE.version;
-
-  /* eslint-disable no-console */
-  if (option.version) {
-    console.log(title);
-    return;
-  }
-  if (option.help) {
-    var cli = require('./lib/cli/cli');
-    console.log(cli.getUsage({
-      title: title,
-      description: PACKAGE.description,
-      footer: 'Project home: [underline]{' + PACKAGE.homepage + '}'
-    }));
-    return;
-  }
-  /* eslint-enable no-console */
-
-  this.log.setLevel(option.log || 'info');
-  this.log.info(title, option);
-
-  var assets = read(option.assets || 'assets.json');
-  var report = this.build(this.prepare(assets));
-  var linked = this.link(report, assets.www);
-  fs.writeFileSync(assets.linked, JSON.stringify(linked));
-
-};
 
 module.exports = tam;
