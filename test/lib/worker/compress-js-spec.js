7bf8bcf05349fd97e9a3362dfc3259fee338992f
arrowrowe <arrowrowe@gmail.com>
2015-10-20 17:30:15 +0800

    Update tests: add test for jsPrefix and jsSuffix

---
+++ test/lib/worker/compress-js-spec.js
@@ -0,0 +1,53 @@
+var compressJs = require('../../../lib/worker/compress-js');
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var stub = sinon.stub;
+var spy = sinon.spy;
+
+var UglifyJS = require('uglify-js');
+
+describe('lib/worker/compress-js', function () {
+
+  var ast = {
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    figure_out_scope: spy(),
+    transform: stub().returns({
+      print_to_string: spy()
+    })
+    // jscs:enable
+  };
+
+  before(function () {
+    stub(UglifyJS, 'parse').returns(ast);
+    stub(UglifyJS, 'Compressor');
+  });
+
+  afterEach(function () {
+    UglifyJS.parse.reset();
+    UglifyJS.Compressor.reset();
+  });
+
+  after(function () {
+    UglifyJS.parse.restore();
+    UglifyJS.Compressor.restore();
+  });
+
+  it('does not add prefix or suffix for default', function () {
+    var code = 'some clean code';
+    compressJs(code, {});
+    expect(UglifyJS.parse.calledWith(code)).to.equal(true);
+  });
+
+  it('supports jsPrefix and jsSuffix and prevent them from being past to compressor', function () {
+    var code = 'some code that need to wrap';
+    compressJs(code, {
+      jsPrefix: '{',
+      jsSuffix: '}',
+      someOtherOption: 42
+    });
+    expect(UglifyJS.parse.calledWith('{' + code + '}')).to.equal(true);
+    expect(UglifyJS.Compressor.calledWith({someOtherOption: 42})).to.equal(true);
+  });
+
+});
