fe2fe2f8ab3d2fd83c30571bb9f97b4f22bcc3a5
arrowrowe <arrowrowe@gmail.com>
2015-10-20 17:31:27 +0800

    Typo: prevent -> prevents

--- test/lib/worker/compress-js-spec.js
+++ test/lib/worker/compress-js-spec.js
@@ -39,7 +39,7 @@ describe('lib/worker/compress-js', function () {
     expect(UglifyJS.parse.calledWith(code)).to.equal(true);
   });
 
-  it('supports jsPrefix and jsSuffix and prevent them from being past to compressor', function () {
+  it('supports jsPrefix and jsSuffix and prevents them from being past to compressor', function () {
     var code = 'some code that need to wrap';
     compressJs(code, {
       jsPrefix: '{',
